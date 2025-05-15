4202a58b3781a9942557452158842c775485e37d
Calvsie <toaofenders@gmail.com>
2025-05-15 16:34:00 -0500

    compliance

--- repl.go
+++ repl.go
@@ -1,10 +1,30 @@
 package main
 
 import(
+	"bufio"
+	"fmt"
+	"os"
 	"strings"
-	)
+)
 
-func 	cleanInput(text string) []string {
+func startRepl() {
+	reader := bufio.NewScanner(os.Stdin)
+	for {
+		fmt.Print("Pokedex > ")
+		reader.Scan()
+
+		words := cleanInput(reader.Text())
+		if len(words) == 0 {
+			continue
+		}
+
+		commandName := words[0]
+
+		fmt.Printf("Your command was: %s\n", commandName)
+	}
+}
+
+func cleanInput(text string) []string {
 	output := strings.ToLower(text)
 	words := strings.Fields(output)
 	return words
