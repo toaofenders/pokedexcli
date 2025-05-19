7985c898df1820adb24d09373ca4a2b2c0866033
Calvsie <toaofenders@gmail.com>
2025-05-19 12:22:21 -0500

    realignment with solution docs

--- internal/pokeapi/client.go
+++ internal/pokeapi/client.go
@@ -3,16 +3,19 @@ package pokeapi
 import (
 	"net/http"
 	"time"
+	"github.com/toaofenders/pokedexcli/internal/pokeapi"
 )
 
 // Client -
 type Client struct {
+	cache	   pokecache.Cache
 	httpClient http.Client
 }
 
 // NewClient -
-func NewClient(timeout time.Duration) Client {
+func NewClient(timeout, cacheInterval time.Duration) Client {
 	return Client{
+		cache: pokecache.NewCache(cacheInterval),
 		httpClient: http.Client{
 			Timeout: timeout,
 		},
